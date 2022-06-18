d5655229b82b9e041a16736a8240b88dd0c9648c
dmmwebcampaizawa <105425062+dmmwebcampaizawa@users.noreply.github.com>
2022-06-18 11:09:20 +0000

    [Update] 3-25-6-18

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,33 @@
 class UsersController < ApplicationController
-
+  
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
+    @book = @user.book
     
   end
 
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+  @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+  
+    private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+  
 end
