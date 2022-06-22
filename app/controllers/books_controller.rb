cd43ee3a45152da204b1ebd1e8e984e878dc3070
dmmwebcampaizawa <105425062+dmmwebcampaizawa@users.noreply.github.com>
2022-06-22 02:03:10 +0000

    [[Finish] 3-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,32 +3,45 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    @books = Book.all
+    @user = current_user
     if @book.save
-      redirect_to book_path(current_user.id)
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
     else
-      render :books_path
+      render :index
     end
   end
-  
+
   def index
     @books = Book.all
     @book = Book.new
+    @user = current_user
   end
 
   def show
     @book = Book.find(params[:id])
+    @book_ = Book.new
+    @user = @book.user
   end
 
   def edit
     @book = Book.find(params[:id])
+    unless @book.user == current_user
+    redirect_to books_path
+    end
   end
-  
+
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(book.id)
+    if @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
+    redirect_to book_path(@book.id)
+    else
+    render :edit
+    end
   end
-  
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
@@ -36,8 +49,8 @@ class BooksController < ApplicationController
   end
 
   private
-  
+
   def book_params
-    params.require(:book).permit(:title, :opinion)
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
   has_one_attached :profile_image, dependent: :destroy
   belongs_to :user
+
+  # バリデーション
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 1, maximum: 200 }
+
 end
+
